c284c6f78ccfc82ffcded930b91e56d2d36b3f61
bennettscience <brian.bennett2@gmail.com>
2019-04-25 10:36:23 -0400

    Add course_id key to Outcome model for filtering

--- app/models.py
+++ app/models.py
@@ -1,12 +1,11 @@
 from app import app, db, login
 from flask_login import UserMixin
 
-alignments = db.Table('alignments',
-    db.Column('outcome_id', db.Integer, db.ForeignKey('outcome.id')),
-    db.Column('assignment_id', db.Integer, db.ForeignKey('assignment.id'))
-)
-
 class User(UserMixin, db.Model):
+    """ User DB Model
+        Store the User object for authentication and matching courses
+
+    """
     __tablename__ = 'Users'
     id = db.Column(db.Integer, primary_key=True)
     canvas_id = db.Column(db.String(64), nullable=False, unique=True)
@@ -16,7 +15,7 @@ class User(UserMixin, db.Model):
     refresh_token = db.Column(db.String)
 
     def __repr__(self):
-        return 'User: {} | {} | {} | {} | {} | {}'.format(self.id, self.canvas_id, self.name, self.token, self.expiration, self.refresh_token)
+        return 'User: {} | {} '.format(self.name, self.canvas_id)
 
 @login.user_loader
 def load_user(id):
@@ -24,14 +23,15 @@ def load_user(id):
 
 class Outcome(db.Model):
     """ Outcome object for the database.
-      id    - unique int
-      title - string name
-      score - binary 0 or 1
+      id : unique int
+      title : string name
+      score : binary 0 or 1
+      courses: list of courses the outcome is assessed in
       """
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(64))
     score = db.Column(db.Integer)
-    
+    course_id = db.Column(db.Integer)
     assignment_id = db.relationship('Assignment', uselist=False, back_populates='outcome')
 
     def align(self, assignment):
@@ -50,17 +50,25 @@ class Outcome(db.Model):
         return self.assignment_id is not None
 
     def __repr__(self):
-        return '< {} || {} || {}>'.format(self.title, self.id, self.assignment_id)
+        return '< {} || {} || Assignment: {} >'.format(self.title, self.id, self.assignments)
 
 class Assignment(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(128))
     score = db.Column(db.Integer)
     course_id = db.Column(db.Integer)
-    # Link to the outcome ID
     outcome_id = db.Column(db.Integer, db.ForeignKey('outcome.id'))
 
     outcome = db.relationship('Outcome', backref='assignment', passive_updates=False, uselist=False)
 
     def __repr__(self):
         return '< {} || {} || {} || {}>'.format(self.id, self.title, self.course_id, self.outcome_id)
+
+
+# # TODO: Figure out how to actually use an association table
+# class OutcomeAssignmentLinks(db.Model):
+#     __tablename__ = 'alignments'
+#     assignment_id = db.Column(db.Integer, db.ForeignKey('assignment.id'), primary_key=True)
+#     outcome_id = db.Column(db.Integer, db.ForeignKey('outcome.id'), primary_key=True)
+#     assignment = db.relationship(Assignment, backref="outcome_link", lazy='dynamic')
+#     outcome = db.relationship(Outcome, backref="assignment_link", lazy='dynamic')
