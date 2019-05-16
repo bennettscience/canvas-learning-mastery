71cb79915f18416f1a87aab35739930ce6a3faf0
bennettscience <brian.bennett2@gmail.com>
2019-05-16 12:54:04 -0400

    Initial commit

--- app/__init__.py
+++ app/__init__.py
@@ -28,20 +28,4 @@ migrate = Migrate(app, db)
 toolbar = DebugToolbarExtension(app)
 
 from app import routes, models
-
-# if not app.debug:
-    # if app.config['MAIL_SERVER']:
-    #     auth = None
-    #     if app.config['MAIL_SERVER'] or app.config['MAIL_PASSWORD']:
-    #         auth = (app.config['MAIL_USERNAME'], app.config['MAIL_PASSWORD'])
-    #     secure = None
-    #     if app.config['MAIL_USE_TLS']:
-    #         secure = ()
-    #     mail_handler = SMTPHandler(
-    #         mailhost=(app.config['MAIL_SERVER'], app.config['MAIL_PORT']),
-    #         fromaddr='no-reply@' + app.config['MAIL_SERVER'],
-    #         toaddrs=app.config['ADMINS'],subject='CanvasDoc Failure',
-    #         credentials=auth, secure=secure)
-    #     mail_handler.setLevel(logging.ERROR)
-    #     app.logger.addHandler(mail_handler)
     
