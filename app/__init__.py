a39e334af7ed1eaa1a9f8d8b71ee7230498ae5a5
bennettscience <brian.bennett2@gmail.com>
2019-08-12 07:58:21 -0400

    fixes SENTRY 13

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,7 @@
 import os
 import logging.handlers
+import sentry_sdk
+from sentry_sdk.integrations.flask import FlaskIntegration
 from flask import Flask, session
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
@@ -11,9 +13,9 @@ from flask_bootstrap import Bootstrap
 os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
 
 # Init logging
-if not os.path.exists('logs'):
-    os.mkdir('logs')
-file_handler = logging.handlers.RotatingFileHandler('logs/canvasdoc.log', mode='a', maxBytes=10240, backupCount=5, encoding='utf-8')
+if not os.path.exists('tmp/logs'):
+    os.mkdir('tmp/logs')
+file_handler = logging.handlers.RotatingFileHandler('tmp/logs/canvasdoc.log', mode='a', maxBytes=10240, backupCount=5, encoding='utf-8')
 file_handler.setFormatter(logging.Formatter(
     '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'
 ))
@@ -30,3 +32,10 @@ toolbar = DebugToolbarExtension(app)
 bootstrap = Bootstrap(app)
 
 from app import routes, models
+
+# Connect to Sentry
+sentry_sdk.init(
+    dsn=app.config['SENTRY_DSN'],
+    integrations=[FlaskIntegration()],
+    release="canvas-mastery@0.1.1"
+)
