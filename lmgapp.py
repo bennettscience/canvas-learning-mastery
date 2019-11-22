3b60b79738799bc747e35669b28ffb986cce6e41
bennettscience <brian.bennett2@gmail.com>
2019-11-22 12:58:25 -0500

    rename base application script

---
+++ lmgapp.py
@@ -0,0 +1,18 @@
+import json
+import requests
+from app import app, db
+from app.models import Outcome, Assignment, User
+from app.server import Outcomes, Assignments
+
+@app.shell_context_processor
+def make_shell_context():
+    return {
+        'db': db,
+        'Outcomes': Outcomes,
+        'Assignments': Assignments,
+        'Outcome': Outcome,
+        'Assignment': Assignment,
+        'User': User,
+        'json': json,
+        'requests': requests
+    }
