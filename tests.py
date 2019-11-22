1e379bceb83f2c261f16aba48487128f5997abc8
bennettscience <brian.bennett2@gmail.com>
2019-11-22 15:01:43 -0500

    trying to be better at testing. New methods for rubric work

--- tests.py
+++ tests.py
@@ -1,9 +1,26 @@
 import unittest
+from canvasapi import Canvas
 from app import app, db
 from app.models import Outcome, Assignment, User
+from app.server import Assignments
+
+# class TestUser(unittest.TestCase):
+
+#     def setUp(self):
+#         app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite://'
+#         db.create_all()
+
+#     def tearDown(self):
+#         db.session.remove()
+#         db.drop_all()
+
+#     def test_new_user(self):
+#         u1 = User(id=1, canvas_id=123, name='Brian Bennett')
+#         db.session.add(u1)
+#         db.session.commit()
 
 
-class UserModelCase(unittest.TestCase):
+class TestOutcomes(unittest.TestCase):
     def setUp(self):
         app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite://'
         db.create_all()
@@ -11,14 +28,23 @@ class UserModelCase(unittest.TestCase):
     def tearDown(self):
         db.session.remove()
         db.drop_all()
-    
-    def
 
     def test_add_outcome(self):
         o1 = Outcome(id=123, title='Some outcome 1', course_id=999)
         o2 = Outcome(id=456, title='Some outcome 2', course_id=888)
         db.session.add_all([o1, o2])
         db.session.commit()
+
+
+class TestAssignments(unittest.TestCase):
+
+    def setUp(self):
+        app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite://'
+        db.create_all()
+
+    def tearDown(self):
+        db.session.remove()
+        db.drop_all()
 
     def test_add_assignment(self):
         a1 = Assignment(id=1, title='Some assignment 1', outcome_id='')
@@ -38,6 +64,21 @@ class UserModelCase(unittest.TestCase):
         db.session.commit()
 
         o1.align(a1)
+
+    def test_get_rubric_result_for_assignment(self):
+        course_id = 36756
+        assignment_id = 166466
+        canvas = Canvas('https://elkhart.instructure.com/', app.config['API']['canvas']['key'])
+        result = Assignments.get_rubric_result_for_assignment(
+            canvas, course_id, assignment_id)
+        self.assertIsInstance(result, list)
+
+    def test_get_course_assignments(self):
+        course_id = 37656
+        canvas = Canvas('https://elkhart.instructure.com', app.config['API']['canvas']['key'])
+
+        result = Assignments.get_course_assignments(canvas, course_id)
+        self.assertIsInstance(result, list)
 
 if __name__ == '__main__':
     unittest.main(verbosity=2)
