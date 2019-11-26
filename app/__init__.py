98f1d616b4fd92a5118b039696b93238492556d8
bennettscience <brian.bennett2@gmail.com>
2019-11-26 14:38:39 -0500

    add missing `routes` import

--- app/__init__.py
+++ app/__init__.py
@@ -13,28 +13,18 @@ from flask_cors import CORS
 
 os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
 
-# Init logging
-# if not os.path.exists('tmp/logs'):
-#     os.mkdir('tmp/logs')
-# file_handler = logging.handlers.RotatingFileHandler('logs/canvasdoc.log', mode='a', maxBytes=10240, backupCount=5, encoding='utf-8')
-# file_handler.setFormatter(logging.Formatter(
-#     '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'
-# ))
-# file_handler.setLevel(logging.INFO)
-
 app = Flask(__name__)
 app.config.from_object(Config)
 app.config['CORS_HEADERS'] = 'Content-Type'
-# app.logger.addHandler(file_handler)
+
 db = SQLAlchemy(app)
 login = LoginManager(app)
 login.login_view = 'index'
 migrate = Migrate(app, db)
-# toolbar = DebugToolbarExtension(app)
 bootstrap = Bootstrap(app)
 CORS(app, resources={r"/student*": {"origins": "https://elkhart.instructure.com/*"}})
 
-from app import routes, models
+from app import app, routes
 
 # Connect to Sentry
 sentry_sdk.init(
