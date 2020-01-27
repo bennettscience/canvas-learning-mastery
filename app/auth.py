caed2b9a1e7815411bb1470e1681fc550b666ca2
bennettscience <brian.bennett2@gmail.com>
2020-01-27 12:58:12 -0500

    remove unnecessary refresh method

--- app/auth.py
+++ app/auth.py
@@ -6,6 +6,7 @@ import time
 
 
 class Auth:
+    """ Manage OAuth flow. """
         
     oauth = OAuth2Session(
         app.config["OAUTH_CREDENTIALS"]["canvas"]["id"],
@@ -20,35 +21,17 @@ class Auth:
         pass
 
     @classmethod
-    def refresh_oauth_token(self):
-        """ Get the user refresh token for API calls
-        :type user: Int
-        :param user: Canvas user ID
-
-        :rtype: Str token
-        """
-        token = self.oauth.fetch_token(
-            app.config["OAUTH_CREDENTIALS"]["canvas"]["token_url"],
-            grant_type="refresh_token",
-            client_id=app.config["OAUTH_CREDENTIALS"]["canvas"]["id"],
-            client_secret=app.config["OAUTH_CREDENTIALS"]["canvas"]["secret"],
-            refresh_token=user.refresh_token,
-        )
-
-        return token
-
-    @classmethod
     def init_canvas(self, token):
         """ Launch a new Canvas object
         :type token: Str
         :param token: OAuth token
 
-        :rtype:
+        :returns canvas: Canvas instance
+        :rtype: Object
         """
         expire = token["expires_at"]
-        app.logger.info("token expires at: %s", expire)
+
         if time.time() > expire:
-            app.logger.info("Requesting a new token from Canvas")
             # get a new access token and store it
             client_id = app.config["OAUTH_CREDENTIALS"]["canvas"]["id"]
             refresh_url = app.config["OAUTH_CREDENTIALS"]["canvas"]["token_url"]
@@ -69,10 +52,12 @@ class Auth:
 
     @classmethod
     def login(self):
+        """Log the user in."""
         return self.auth_url
 
     @classmethod
     def get_token(self):
+        """ Retrieve an access token from Canvas. """
         token = self.oauth.fetch_token(
             app.config["OAUTH_CREDENTIALS"]["canvas"]["token_url"],
             client_secret=app.config["OAUTH_CREDENTIALS"]["canvas"]["secret"],
